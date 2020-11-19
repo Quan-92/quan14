f705ea63ab5777e84c9dabf836f4d1c1f16a481a
Quan-92 <9602tranquan@gmail.com>
2020-11-19 09:00:33 +0700

    run

--- Admin/WebstormProjects/gochoctap/src/Detail.js
+++ Admin/WebstormProjects/gochoctap/src/Detail.js
@@ -10,6 +10,10 @@ export default function Detail() {
             <div className="container">
                 <article className="content">
                     <div className="body-content">
+                        <div style={{width:"1000px",height:"500px"}}>
+                            <GoogleMap center={center} zoom={zoom}
+                                       bootstrapURLKeys="AIzaSyCaj0mHlR-keu-9hsR6d-gB0L9BclG04rk"/>
+                        </div>
                         <h1>Đây là 1 bài báo hay</h1>
                         <img src="imgs/iphone_x_64gb_3.jpg" height={600} width={600} />
                     </div>
